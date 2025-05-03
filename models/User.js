3e5f80d54ff01edfe3bfe10fd7d20892e95b4b39
aathifdev <aathifahd.dev@gmail.com>
2025-05-03 16:51:07 +0530

    First commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id:{ type : String, required:true},
+    name: { type : String, required:true},
+    emai:{ type : String, required:true, unique:true},
+    imageUrl: {type : String, required:true},
+    cardItems: {type:Object, default:{} }
+}, {minimize: false})
+
+const user = mongoose.models.user || mongoose.model('user',userSchema)
+
+export default User
