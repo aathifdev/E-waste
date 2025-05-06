563cc4fe8feaff363f8f1dd2c5846649a6db65bb
aathifdev <aathifahd.dev@gmail.com>
2025-05-06 07:57:44 +0530

    mongoDB bug fixing

--- app/api/user/data/route.js
+++ app/api/user/data/route.js
@@ -1,26 +1,30 @@
 import connectDB from "@/config/db";
 import User from '@/models/User';
-import { auth, } from "@clerk/nextjs/server";
+import { clerkClient, getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
 export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
 
-    try {
-        const {userId} = auth(request)
+    await connectDB();
 
-        await connectDB()
-        const user = await User.findOne({ clerkId: userId})
+    let user = await User.findById(userId);
 
-        if (!user) {
-            return NextResponse.json({ success: false, message: "User Not Found"})
-        }
+    if (!user) {
+      const clerkUser = await clerkClient.users.getUser(userId);
 
-        return NextResponse.json({success:true, user})
-
-    } catch (error) {
-        console.error('Api Error:', error);
-        return NextResponse.json({ success: false, message: error.message})
+      user = await User.create({
+        _id: userId,
+        name: clerkUser.firstName || 'No Name',
+        email: clerkUser.emailAddresses[0]?.emailAddress || '',
+        imageUrl: clerkUser.imageUrl,
+      });
     }
-    
+
+    return NextResponse.json({ success: true, user });
+
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
 }
